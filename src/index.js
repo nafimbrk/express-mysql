dc63e5ed9c33a344a16073d74324844f0f1f84e0
nafimbrk <nafimubarok2005@gmail.com>
2025-04-18 19:19:12 -0700

    upload file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const express = require("express");
 
 const userRoutes = require('./routes/users')
 
-const midllewareLogRequest = require('./middleware/logs')
+const midllewareLogRequest = require('./middleware/logs');
+const upload = require('./middleware/multer');
 
 
 
@@ -12,16 +13,19 @@ const app = express();
 
 app.use(midllewareLogRequest)
 app.use(express.json())
-
-app.use((req, res, next) => {
-    console.log('middleware ke dua')
-    next()
-})
+app.use('/assets', express.static('public/images'))
 
 app.use('/users', userRoutes)
+app.post('/upload', upload.single('photo'), (req, res) => {
+  res.json({
+    message: 'upload behasil'
+  })
+})
 
-app.use('/', (req, res) => {
-  
+app.use((err, req, res, next) => {
+  res.json({
+    message: err.message
+  })
 })
 
 app.listen(PORT, () => {
